fa76688fbeec43f806564bce1b4fde142582e68d
mengqp <meng_qingpu@126.com>
2015-09-18 14:48:31 +0800

    10进制专16进制
    将一段16进制的字符串转化为16进制的数字
    第一次写python代码，代码量是少了不少，以后多联系

---
+++ pycode/atoh/atoh.py
@@ -0,0 +1,28 @@
+def atoh( szBuf, length, byflag ):
+    i=0
+    tempvalue=0
+    value=0
+    if 8  <= length:
+        length=8 
+    
+    while i<length:
+        if 1==byflag:
+            j=i
+        elif 0 == byflag:
+            j=length-1
+
+        if szBuf[j] >= 'A' and szBuf[j]  <= 'F':
+            tempvalue = int ( szBuf[j],  16 ) - int( 'A' , 16) + 10
+        if szBuf[j]  >= 'a' and szBuf[j] <= 'f':
+            tempvalue = int ( szBuf[j], 16 ) - int( 'a',  16 ) + 10
+        if szBuf[j]  >= '0' and szBuf[j] <= '9':
+            tempvalue = int ( szBuf[j], 16 ) - int( '0',  16 )
+
+        value = value*16+tempvalue 
+        i+=1
+
+    return value
+
+buf = "0a0b0c0z050607"
+print(atoh(buf, 2, 1))
+input()
